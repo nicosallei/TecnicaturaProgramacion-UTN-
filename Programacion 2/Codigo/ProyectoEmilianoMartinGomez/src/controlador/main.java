bc4861d3da6161c4fff6d5e4d2d6886763bdfb5b
Sallei <nicosallei@gmail.com>
2023-05-30 00:02:50 -0300

    actualizacion

---
+++ Programacion 2/Codigo/ProyectoEmilianoMartinGomez/src/controlador/main.java
@@ -0,0 +1,36 @@
+package controlador;
+
+import vistas.Loading;
+import vistas.menu;
+
+/**
+ *
+ * @author SkylakeFrost
+ */
+public class main {
+
+    public static void main(String[] args) {
+
+         long time_start, time_end;
+        time_start = System.currentTimeMillis();
+        Loading load = new Loading();
+
+        load.setVisible(true);  //Se Abre El Splash De Carga y Se Inician Funciones...
+
+        menu menu = new menu();
+        ControladorMenuPrincipal cm = new ControladorMenuPrincipal(menu);
+        cm.panelAlumno();// Por Defecto Inicia En El Panel De Alumnos
+        menu.setLocationRelativeTo(null);
+        menu.setResizable(false);
+        menu.setTitle("Proyecto Final - Programacion II");
+        menu.getSelectAlumnos().setVisible(true);
+        menu.getLblMainTitle().setText("Lista De Alumnos");
+
+        load.setVisible(false);//Se Cierra El Splash De Carga
+
+        menu.setVisible(true);//Se Muestra El Menú Con Los Datos Cargados
+        time_end = System.currentTimeMillis();
+
+        System.out.println("Tiempo De Carga : " + (time_start - time_end));
+    }
+}
