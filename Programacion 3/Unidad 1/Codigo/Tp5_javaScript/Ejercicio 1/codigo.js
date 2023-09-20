a35a6357187a2644d52f31bbd08f8b36a8e84932
Sallei <nicosallei@gmail.com>
2023-09-20 17:56:23 -0300

    actualizacion

---
+++ Programacion 3/Unidad 1/Codigo/Tp5_javaScript/Ejercicio 1/codigo.js
@@ -0,0 +1,9 @@
+
+//crea una varaible donde le asigna un mensaje
+var mensaje = "Hola Mundo!";
+alert(mensaje);
+
+//crea una variable donde le asigna un mensaje.
+var segundoMensaje = "Soy el primer script";
+//muestra un cuadro de diálogo con el contenido de la variable segundoMensaje, que en este caso es "Soy el primer script".
+alert(segundoMensaje);
